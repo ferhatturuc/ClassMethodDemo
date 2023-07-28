48db69a9e8d2084df60e60e4cd281f589a3cd13a
4862348 <4862348@UBILYZLSMD017>
2023-07-28 10:21:33 +0300

    commit

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,23 +6,23 @@
         {
             Customer customer1 = new Customer();
             customer1.Id = 1;
-            customer1.Name = "Kübra";
-            customer1.Address = "Bursa";
+            customer1.Name = "Ferhat";
+            customer1.Address = "Konya";
 
             Customer customer2 = new Customer();
             customer2.Id = 2;
-            customer2.Name = "Necati";
-            customer2.Address = "Manisa";
+            customer2.Name = "Faruk";
+            customer2.Address = "Samsun";
 
             Customer customer3 = new Customer();
             customer3.Id = 3;
-            customer3.Name = "Merve";
-            customer3.Address = "Muş";
+            customer3.Name = "Fatih";
+            customer3.Address = "Kayseri";
 
             Customer customer4 = new Customer();
             customer4.Id = 4;
-            customer4.Name = "Can";
-            customer4.Address = "Malatya";
+            customer4.Name = "Muhammet";
+            customer4.Address = "Samsun";
 
 
             Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
